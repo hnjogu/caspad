bdcc83ad963bf136050f9bb9e2506f27c74cc7ab
erickie007 <erickie007@outlook.com>
2020-06-24 15:29:57 +0300

    upload

--- resources/views/projects/findwork.blade.php
+++ resources/views/projects/findwork.blade.php
@@ -38,7 +38,8 @@
                         <td> {{$row->subject}} </td>
                         <td>
                             <video id="myVideo" width="300" height="100" controls>
-                              <source src="{{asset('/files/' .$row->file_name)}}" type="audio/ogg">
+                              <!-- <source src="{{asset('/files/' .$row->file_name)}}" type="audio/ogg"> -->
+                              <source src="{{url('/files/' .$row->file_name)}}" type="audio/ogg">
                               Your browser does not support HTML5 video.
                             </video>
 {{--                             <audio controls>
